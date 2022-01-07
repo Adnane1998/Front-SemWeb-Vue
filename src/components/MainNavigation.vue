81306b680e4b62cb22aa48ccbc77129fd1e85246
eleves <eleves@adm-pret-07.emse2000.local>
2022-01-07 03:03:17 +0100

    Update

--- src/components/MainNavigation.vue
+++ src/components/MainNavigation.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="nav nav-tabs">
     <li class="nav-item">
-      <a class="nav-link active" aria-current="page" href="#">Windows</a>
+      <a class="nav-link active" aria-current="page" href="#">Rooms</a>
     </li>
    
   </ul>
